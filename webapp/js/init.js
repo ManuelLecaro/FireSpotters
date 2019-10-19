3a7b67096c41a05efcb12e87ec374ff81501561c
Manuel Lecaro <mnlecaro@gmail.com>
2019-10-19 15:43:47 -0500

    changes

--- webapp/js/init.js
+++ webapp/js/init.js
@@ -4,3 +4,109 @@ wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+
+
+// Create a WorldWindow for the canvas.
+var wwd = new WorldWind.WorldWindow("canvasOne");
+
+wwd.addLayer(new WorldWind.BMNGOneImageLayer());
+wwd.addLayer(new WorldWind.BMNGLandsatLayer());
+
+wwd.addLayer(new WorldWind.CompassLayer());
+wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
+wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+
+// Add a placemark
+var placemarkLayer = new WorldWind.RenderableLayer();
+wwd.addLayer(placemarkLayer);
+
+var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+
+placemarkAttributes.imageOffset = new WorldWind.Offset(
+    WorldWind.OFFSET_FRACTION, 0.3,
+    WorldWind.OFFSET_FRACTION, 0.0);
+
+placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+    WorldWind.OFFSET_FRACTION, 0.5,
+    WorldWind.OFFSET_FRACTION, 1.0);
+
+placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";
+
+var position = new WorldWind.Position(55.0, -106.0, 100.0);
+var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+
+placemark.label = "Placemark\n" +
+    "Lat " + placemark.position.latitude.toPrecision(4).toString() + "\n" +
+    "Lon " + placemark.position.longitude.toPrecision(5).toString();
+placemark.alwaysOnTop = true;
+
+placemarkLayer.addRenderable(placemark);
+
+// Add a polygon
+var polygonLayer = new WorldWind.RenderableLayer();
+wwd.addLayer(polygonLayer);
+
+var polygonAttributes = new WorldWind.ShapeAttributes(null);
+polygonAttributes.interiorColor = new WorldWind.Color(0, 1, 0.5, 0.75);
+polygonAttributes.outlineColor = WorldWind.Color.GREEN;
+polygonAttributes.drawOutline = true;
+polygonAttributes.applyLighting = true;
+
+var boundaries = [];
+boundaries.push(new WorldWind.Position(20.0, -75.0, 700000.0));
+boundaries.push(new WorldWind.Position(25.0, -85.0, 700000.0));
+boundaries.push(new WorldWind.Position(20.0, -95.0, 700000.0));
+
+var polygon = new WorldWind.Polygon(boundaries, polygonAttributes);
+polygon.extrude = true;
+polygonLayer.addRenderable(polygon);
+
+// Add a COLLADA model
+var modelLayer = new WorldWind.RenderableLayer();
+wwd.addLayer(modelLayer);
+
+///Collada model
+var position = new WorldWind.Position(10.0, -125.0, 800000.0);
+var config = {dirPath: 'http://localhost:8887/images/Tree01/3dmodel/'};
+
+
+var colladaLoader = new WorldWind.ColladaLoader(position, config);
+colladaLoader.load("tree01.dae", function (colladaModel) {
+    colladaModel.scale = 9000;
+    console.log(colladaModel)
+    modelLayer.addRenderable(colladaModel);
+});
+
+//var position2 = new WorldWind.Position(15.0, -122.0, 800000.0);
+//var config2 = 'http://localhost:8887/images/Tree01/3dmodel/'
+//var tree_collada = new WorldWind.ColladaLoader(position2, config2);
+//tree_collada.load('tree01.dae',function(colladaModel){
+//    colladaModel.scale = 8000;
+//    modelLayer.addRenderable(colladaModel)
+//})
+//
+//
+//// Add WMS imagery
+var serviceAddress = "https://firms.modaps.eosdis.nasa.gov/wms/?SERVICE=WMS&VERSION=1.1.1&REQUEST=GETCAPABILITIES";
+var layerName = "MOD_LSTD_CLIM_M";
+
+var createLayer = function (xmlDom) {
+    var wms = new WorldWind.WmsCapabilities(xmlDom);
+    var wmsLayerCapabilities = wms.getNamedLayer(layerName);
+    var wmsConfig = WorldWind.WmsLayer.formLayerConfiguration(wmsLayerCapabilities);
+    var wmsLayer = new WorldWind.WmsLayer(wmsConfig);
+    wwd.addLayer(wmsLayer);
+};
+
+var logError = function (jqXhr, text, exception) {
+    console.log("There was a failure retrieving the capabilities document: " +
+        text +
+    " exception: " + exception);
+};
+
+$.get(serviceAddress).done(createLayer).fail(logError);
+
+
+
+
+
